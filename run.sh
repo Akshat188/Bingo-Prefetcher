a186b41e7967eede7c31959f7f3aa64d4540d464
Akshat188 <akshat.gupta@bytelearn.ai>
2021-11-25 00:41:37 +0530

    Run on Benchmarks

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+benchmarks=(600.perlbench_s-570B.champsimtrace.xz 605.mcf_s-994B.champsimtrace.xz 620.omnetpp_s-141B.champsimtrace.xz 641.leela_s-1083B.champsimtrace.xz 657.xz_s-2302B.champsimtrace.xz)
+
+cd ChampSim
+
+#baseline
+./build_champsim.sh bimodal no no no no lru 1
+
+for i in "${benchmarks[@]}"
+do
+	./run_champsim.sh bimodal-no-no-no-no-lru-1core 10 10 "$i"
+done
+
+
+#with Bingo
+./build_champsim.sh bimodal no bingo_dpc3 no no lru 1
+
+for i in "${benchmarks[@]}"
+do
+	./run_champsim.sh bimodal-no-bingo_dpc3-no-no-lru-1core 10 10 "$i"
+done
+
